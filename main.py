cafb23ae9e5f216f59a8387ba62a7543e7fc2a36
JonaSavage <jonathan.stickle@gmail.com>
2026-03-29 13:55:13 -0700

    Chapter 1 complete

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import argparse
 
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("API key is required")
+
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.prompt}\n")
+
+    generate_content(client=client, messages=messages, verbose=args.verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    print("\nResponse:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
